de78cd3ada8b631dfc4652ddb50e8cbfd2751cd8
Ivan Kozlov <ivaan.kozlov@gmail.com>
2020-04-22 19:27:54 -0700

    add intial app

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React from "react";
+
+import {
+  ErrorBoundary,
+  Facet,
+  SearchProvider,
+  WithSearch,
+  SearchBox,
+  Results,
+  PagingInfo,
+  ResultsPerPage,
+  Paging,
+  Sorting
+} from "@elastic/react-search-ui";
+import { Layout, SingleSelectFacet } from "@elastic/react-search-ui-views";
+import "@elastic/react-search-ui-views/lib/styles/styles.css";
+
+import buildRequest from "./buildRequest";
+import runRequest from "./runRequest";
+import applyDisjunctiveFaceting from "./applyDisjunctiveFaceting";
+import buildState from "./buildState";
+
+const config = {
+  debug: true,
+  hasA11yNotifications: true,
+  onResultClick: () => {
+    /* Not implemented */
+  },
+  onAutocompleteResultClick: () => {
+    /* Not implemented */
+  },
+  onAutocomplete: async ({ searchTerm }) => {
+    const requestBody = buildRequest({ searchTerm });
+    const json = await runRequest(requestBody);
+    const state = buildState(json);
+    return {
+      autocompletedResults: state.results
+    };
+  },
+  onSearch: async state => {
+    const { resultsPerPage } = state;
+    const requestBody = buildRequest(state);
+    // Note that this could be optimized by running all of these requests
+    // at the same time. Kept simple here for clarity.
+    const responseJson = await runRequest(requestBody);
+    const responseJsonWithDisjunctiveFacetCounts = await applyDisjunctiveFaceting(
+      responseJson,
+      state,
+      ["visitors", "states"]
+    );
+    return buildState(responseJsonWithDisjunctiveFacetCounts, resultsPerPage);
+  }
+};
+
+export default function App() {
+  return (
+    <SearchProvider config={config}>
+      <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
+        {({ wasSearched }) => (
+          <div className="App">
+            <ErrorBoundary>
+              <Layout
+                header={
+                  <SearchBox
+                    autocompleteMinimumCharacters={3}
+                    autocompleteResults={{
+                      linkTarget: "_blank",
+                      sectionTitle: "Results",
+                      titleField: "title",
+                      urlField: "link",
+                      shouldTrackClickThrough: true,
+                      clickThroughTags: ["test"]
+                    }}
+                    autocompleteSuggestions={true}
+                  />
+                }
+                bodyContent={
+                  <Results
+                    titleField="title"
+                    urlField="nps_link"
+                    shouldTrackClickThrough={true}
+                  />
+                }
+                bodyHeader={
+                  <React.Fragment>
+                    {wasSearched && <PagingInfo />}
+                    {wasSearched && <ResultsPerPage />}
+                  </React.Fragment>
+                }
+                bodyFooter={<Paging />}
+              />
+            </ErrorBoundary>
+          </div>
+        )}
+      </WithSearch>
+    </SearchProvider>
+  );
+}
